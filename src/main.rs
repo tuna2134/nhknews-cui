025ec12a9e4eddbe1d86385a8e4594308863ab3b
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-05-31 20:42:06 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use tui::{backend::CrosstermBackend, Terminal};
+
+fn main() -> Result<(), io::Error> {
+    let stdout = io::stdout().into_raw_mode()?;
+    let backend = TermionBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    Ok(())
 }
